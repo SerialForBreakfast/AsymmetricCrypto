6ccb75d922f869591885c552330f8ff98b2d586c
Joe McCraw <joe@showblender.com>
2020-04-01 13:25:41 -0700

    Added Facade to keychain

--- AsymmetricCrypto/ViewController.swift
+++ AsymmetricCrypto/ViewController.swift
@@ -13,8 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testEncryptDecrypt()
     }
 
-
+    private func testEncryptDecrypt() {
+        let facade = KeychainFacade()
+        let text = "Impossible to guess data."
+        
+        do {
+            if let encryptedData = try facade.encrypt(text: text) {
+                print("Text Encryption Successful")
+                if let decryptedData = try facade.decrypt(data: encryptedData) {
+                    print("Data decrypted successfully")
+                    print("decrypted data: \(String(data: decryptedData, encoding: .utf8) ?? "")")
+                }
+            }
+        } catch {
+            print("error: \(error)")
+        }
+    }
 }
 
